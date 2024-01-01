7e4a75504451c183212076f10cbf31586853a9b5
toddLiao469469 <toddliao.dev@gmail.com>
2024-01-01 21:39:55 +0800

    refactor: refactor for sotre

--- src/lib/views/PyroxeneResult.svelte
+++ src/lib/views/PyroxeneResult.svelte
@@ -1,49 +1,50 @@
 <script lang="ts">
-	import { MonthlyCard } from '$lib/types';
-	import { getMonthlyCardPyroxene, getSystemPyroxene } from '$lib/utils';
+	import {
+		pyroxene,
+		totalPyroxene,
+		systemPyroxene,
+		pyroxeneOfMonthlyCard,
+		pyroxeneOfArena,
+		daysOfCalculation
+	} from '$store/pyroxene';
 
-	export let initPyroxene: number;
-	export let monthCard: MonthlyCard;
-	export let days: number;
-	export let pyroxeneOfArena: number;
 
-	export let totalPyroxene: number;
 </script>
 
 <div>
 	<div class="grid grid-cols-2">
 		<p class="col-span-2">
-			總共存天數：{days}
+			總共存天數：{$daysOfCalculation}
 		</p>
 
 		<p class="text-cyan-500">
-			月卡石頭 : {getMonthlyCardPyroxene(monthCard, days)}
+			月卡石頭 : {$pyroxeneOfMonthlyCard}
 		</p>
 
 		<p class="text-yellow-500">
-			競技場石頭 : {pyroxeneOfArena}
+			競技場石頭 : {$pyroxeneOfArena}
 		</p>
 		<p class="col-span-2 text-teal-500">
-			每日任務＋登入＋每週任務石頭 : {getSystemPyroxene(days)}
+			每日任務＋登入＋每週任務石頭 : {$systemPyroxene}
 		</p>
 
 		<p class="col-span-2">
 			總共石頭 :
-			{initPyroxene}
+			{$pyroxene.initPyroxene}
 			+
 			<span class="text-teal-500">
-				{getSystemPyroxene(days)}
+				{$systemPyroxene}
 			</span>
 			+
 			<span class="text-cyan-500">
-				{getMonthlyCardPyroxene(monthCard, days)}
+				{$pyroxeneOfMonthlyCard}
 			</span>
 			+
 			<span class="text-yellow-500">
-				{pyroxeneOfArena}
+				{$pyroxeneOfArena}
 			</span>
 			=
-			{totalPyroxene}
+			{$totalPyroxene}
 		</p>
 	</div>
 </div>
--- src/routes/pyroxene-calculator/+page.svelte
+++ src/routes/pyroxene-calculator/+page.svelte
@@ -1,62 +1,52 @@
 <script lang="ts">
+	import { format } from 'date-fns/fp';
+
 	import Calendar from '$lib/components/Calendar.svelte';
 	import { Input } from '$lib/components/ui/input';
-	import { differenceInDays, format, addDays } from 'date-fns/fp';
-	import { type DateValue, fromDate } from '@internationalized/date';
 	import { Label } from '$lib/components/ui/label';
+	import { Slider } from '$lib/components/ui/slider';
+
 	import RaidRankSelect from '$lib/components/RaidRankSelect.svelte';
-	import { MonthlyCard, RaidRank } from '$lib/types';
 	import MonthlyCardSelect from '$lib/components/MonthlyCardSelect.svelte';
-	import { getMonthlyCardPyroxene, getSystemPyroxene } from '$lib/utils';
 	import ArenaRankSelect from '$lib/components/ArenaRankSelect.svelte';
 	import PyroxeneResult from '$lib/views/PyroxeneResult.svelte';
 	import EventTable from '$lib/views/EventTable.svelte';
+	import { pyroxene, pyroxeneOfEvents } from '$store/pyroxene';
 
 	const today = new Date();
-	let initPyroxene = 24_000;
-	let dailyPyroxeneOfArena: string;
-	let daysValue: DateValue = fromDate(addDays(7)(today), 'Asia/Taipei');
-	let raidRank: RaidRank = RaidRank.RANK_1;
-	let monthCard: MonthlyCard = MonthlyCard.BOTH;
-
-	$: days = differenceInDays(today)(daysValue?.toDate('Asia/Taipei'));
-	$: pyroxeneOfArena = Number(dailyPyroxeneOfArena) * days;
-
-	$: totalPyroxene =
-		Number(initPyroxene) +
-		getMonthlyCardPyroxene(monthCard, days) +
-		pyroxeneOfArena +
-		getSystemPyroxene(days);
-
-	$: console.log('initPyroxene', initPyroxene, daysValue?.toDate('Asia/Taipei'), raidRank);
+	$: console.log($pyroxeneOfEvents , $pyroxene.questCompletedRate);
 </script>
 
-<div>
-	<h1 class="scroll-m-20 tracking-tight text-4xl font-600">存石計算機</h1>
+<div class="px-4 w-full md:w-4/5 lg:w-2/3 mx-auto">
+	<div class="flex justify-center flex-col">
+		<h1 class="scroll-m-20 tracking-tight text-4xl font-600">存石計算機</h1>
+		<p>
+			今天是 {format('yyyy-MM-dd')(today)}
+		</p>
 
-	<p>
-		今天是 {format('yyyy-MM-dd')(today)}
-	</p>
+		<div class="grid gap-y-12 items-center mt-2">
+			<div class="grid grid-cols-2 gap-y-2">
+				<Label for="initPyroxene">現在擁有的石頭</Label>
+				<Input class="w-full" type="number" bind:value={$pyroxene.initPyroxene} />
 
-	<div class="grid max-w-xl gap-y-12 items-center px-4">
-		<div class="grid grid-cols-2 gap-y-2">
-			<Label for="initPyroxene">現在擁有的石頭</Label>
-			<Input class="w-full" type="number" bind:value={initPyroxene} />
+				<Label for="daysValue">要存到的日期</Label>
+				<Calendar bind:value={$pyroxene.targetDate}></Calendar>
 
-			<Label for="daysValue">要存到的日期</Label>
-			<Calendar bind:value={daysValue}></Calendar>
+				<Label>競技場每日石頭</Label>
+				<ArenaRankSelect bind:value={$pyroxene.dailyPyroxeneOfArena} />
 
-			<Label>競技場每日石頭</Label>
-			<ArenaRankSelect bind:value={dailyPyroxeneOfArena} />
+				<Label>總力戰檔位</Label>
+				<RaidRankSelect bind:value={$pyroxene.raidRank} />
 
-			<Label>總力戰檔位</Label>
-			<RaidRankSelect bind:value={raidRank} />
+				<Label>有無買月卡</Label>
+				<MonthlyCardSelect bind:value={$pyroxene.monthlyCard} />
 
-			<Label>有無買月卡</Label>
-			<MonthlyCardSelect bind:value={monthCard} />
+				<Label>活動挑戰任務完成比率 : {$pyroxene.questCompletedRate}%</Label>
+				<Slider bind:value={$pyroxene.questCompletedRate} max={100} step={1} />
+			</div>
+
+			<PyroxeneResult />
+			<EventTable />
 		</div>
-
-		<PyroxeneResult {initPyroxene} {monthCard} {days} {pyroxeneOfArena} {totalPyroxene} />
-		<EventTable value={daysValue?.toDate('Asia/Taipei')} {raidRank} />
 	</div>
 </div>
