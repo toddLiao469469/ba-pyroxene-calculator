24747b755cd052dce6aa3b8baf6063f0686b6fed
toddLiao469469 <toddliao.dev@gmail.com>
2024-01-12 00:02:03 +0800

    feat: add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
-	import  Header  from '$lib/components/Header.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 	import './styles.css';
 
 	import { ModeWatcher } from 'mode-watcher';
@@ -7,9 +8,9 @@
 
 <div class="app">
 	<ModeWatcher />
-	
 	<Header />
 	<main>
 		<slot />
 	</main>
+	<Footer />
 </div>
