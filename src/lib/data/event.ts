81f708f230d32fb0c33c63582cd592e13e5fe366
ToddLiao <toddliao.dev@gmail.com>
2024-01-21 13:23:14 +0800

    Refactor/modify data file (#4)
    * refactor: add event.json
    
    * refactor: add zod to validate event data

--- src/lib/data/event.ts
+++ src/lib/data/event.ts
@@ -1,3 +1,6 @@
+import { z } from 'zod';
+import eventData from './event.json';
+
 export enum EventType {
 	Normal = 'NORMAL',
 	Raid = 'RAID',
@@ -34,83 +37,35 @@ export interface IEvent {
 	raidLocationType?: RaidLocationType;
 }
 
-export const events: IEvent[] = [
-	{
-		date: '2024-01-09',
-		name: '維護',
-		pyroxene: 600,
-		eventType: EventType.Normal
-	},
-	{
-		date: '2024-01-09',
-		name: '室内葛利果',
-		eventType: EventType.Raid,
-		RaidPyroxeneType: RaidPyroxeneType.Ranking,
-		raidArmorType: RaidArmorType.Light,
-		raidLocationType: RaidLocationType.Indoor,
-		description: '結算'
-	},
-	{
-		date: '2024-01-16',
-		name: '市街壽司',
-		pyroxene: 650,
-		eventType: EventType.EliminationRaid,
-		RaidPyroxeneType: RaidPyroxeneType.Basic,
-		description: '大決戰獎勵'
-	},
-	{
-		date: '2024-01-23',
-		name: '【復刻】阿拜多斯渡假村復舊對策委員會',
-		pyroxene: 4240,
-		eventType: EventType.Normal,
-		description: '基礎 + 維護'
-	},
-	{
-		date: '2024-01-24',
-		name: '【復刻】阿拜多斯渡假村復舊對策委員會',
-		pyroxene: 650,
-		eventType: EventType.Challenge,
-		description: '挑戰'
-	},
-	{
-		date: '2024-01-30',
-		name: '尋找隱藏的遺產～\n三一的課外活動～',
-		pyroxene: 4240,
-		eventType: EventType.Normal,
-		description: '基礎 + 維護'
-	},
-	{
-		date: '2024-01-31',
-		name: '尋找隱藏的遺產～\n三一的課外活動～',
-		pyroxene: 650,
-		eventType: EventType.Challenge,
-		description: '挑戰'
-	},
+const eventSchema = z.object({
+	date: z.string(),
+	name: z.string(),
+	pyroxene: z.number().optional(),
+	eventType: z.nativeEnum(EventType),
+	RaidPyroxeneType: z.nativeEnum(RaidPyroxeneType).optional(),
+	description: z.string().optional(),
+	raidArmorType: z.nativeEnum(RaidArmorType).optional(),
+	raidLocationType: z.nativeEnum(RaidLocationType).optional()
+});
 
-	{
-		date: '2024-02-06',
-		name: '7日登入獎勵\n 「2.5周年記念ログインボーナス」',
-		pyroxene: 1250,
-		eventType: EventType.Normal,
-		RaidPyroxeneType: RaidPyroxeneType.Basic
-	},
-	{
-		date: '2024-02-13',
-		name: '室外GOZ',
-		eventType: EventType.Raid,
-		pyroxene: 1250,
-		RaidPyroxeneType: RaidPyroxeneType.Basic,
-		raidArmorType: RaidArmorType.Special,
-		raidLocationType: RaidLocationType.Outdoor,
-		description: '總力戰基礎+成就'
-	},
-	{
-		date: '2024-02-13',
-		name: '室外GOZ',
-		eventType: EventType.Raid,
-		RaidPyroxeneType: RaidPyroxeneType.Ranking,
-		raidArmorType: RaidArmorType.Special,
-		raidLocationType: RaidLocationType.Outdoor,
-		description: '結算'
+const eventsSchema = z.array(eventSchema);
+
+export const validateEvent = (data: unknown): data is IEvent[] => {
+	try {
+		eventsSchema.parse(data);
+		return true;
+	} catch (error) {
+		console.error(error, 'Event data is invalid');
+		return false;
 	}
-];
+};
+
+const getEvents = (): IEvent[] => {
+	if (validateEvent(eventData)) {
+		return eventData;
+	}
+
+	throw new Error('Event data is invalid');
+};
+
+export const events = getEvents();
