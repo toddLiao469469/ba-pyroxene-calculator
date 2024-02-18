dae9c7d251e826a7ca7b2f04773ea77fb156cf4e
toddLiao469469 <toddliao.dev@gmail.com>
2024-02-19 01:01:00 +0800

    fix: improvement event table ui

--- src/lib/views/EventTable.svelte
+++ src/lib/views/EventTable.svelte
@@ -4,6 +4,7 @@
 	import { pyroxene } from '$store/pyroxene';
 
 	const today = new Date().setHours(0, 0, 0, 0);
+	const targetDate = $pyroxene.targetDate;
 	const includedEvents = events.filter(({ date }) => {
 		return new Date(date).setHours(0, 0, 0, 0) >= today;
 	});
@@ -12,8 +13,8 @@
 <div>
 	<div class="grid grid-cols-12 gap-x-2 gap-y-4 max-w-4xl">
 		<p class="hidden col-span-1 lg:block">日期</p>
-		<p class="hidden col-span-1 lg:block">天數</p>
-		<p class="col-span-2 lg:hidden">日期 <br /> 天數</p>
+		<p class="hidden col-span-1 lg:block">距離今天</p>
+		<p class="col-span-2 lg:hidden">日期 <br /> 距離今天</p>
 
 		<p class="col-span-6">活動名稱</p>
 		<p class="col-span-2">基礎值</p>
@@ -25,6 +26,7 @@
 			class="grid grid-cols-12 gap-x-2 gap-y-4 max-w-4xl min-h-14  items-center"
 			{index}
 			{event}
+			{targetDate}
 			raidRank={$pyroxene.raidRank}
 		/>
 	{/each}
